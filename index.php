f8ca1c3d29f67db7df4a258ae428e620b9f80047
Npatil0211 <86281195+Npatil0211@users.noreply.github.com>
2022-08-02 16:58:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Educational Management System</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <h1 class="my-4">Welcome to Educational Management System</h1>
+
+        <!-- Marketing Icons Section -->
+            <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Latest Event</h4>
+                   
+                        <p class="card-text" style="padding-left:2%"><p>8th Annual General Meeting of IMR Alumni Association is on 13th November 2021 at 04.00 pm @ IMR Conference Hall</p>
+                    <br>FB Live program with Mr. Rahul Narvekar on Startup opportunities<p></p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Eminent Personality</h4>
+                   
+                        <p class="card-text" style="padding-left:2%"><b>Dr. Anil Sahasrabudhe</b>:-
+On the Occasion of Amrut Mahotsav of KCE Society, Open Discussion with AICTE Chairman Dr. Anil Sahasrabuddhe with the Management Representatives, Principals, Directors and Faculty members of AICTE Approved Colleges and Institutes was organised on 15th September 2019.A very healthy discussion was carried on a range of issues between the AICTE Chairman and invited guests.
+<br><b>Mr. Rahul Solapurkar</b> :-
+                    A special Inspirational lecture series on " Swami Vivekanand" and "Management Practices of Shivaji Maharaj" by Renowned artist and Speaker Mr. Rahul Solapur was organised by KCES's IMR at Kantai Hall on 16th and 17th January 2019.
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header">Notices& Announcement</h4>
+                   
+                        <p class="card-text" style="padding-left:2%">Tentative dates for MBA-CET 2020 is 24-25-26 August 2022 for details click here
+                            <br><br>NAAC Student Satisfaction Survey
+View File</p>
+                </div>
+            </div>
+        </div><!---->
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <h2>Availablity Of Degree/Post Degree :</h2>
+
+        <div class="row">
+                   
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/degree.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title">M.C.A.</h4>
+<p class="card-text"><b>  Intake :&nbsp; 120</b></p>
+<p class="card-text"><b>Faculty Co-Ordinator :Mr. UDAY CHATUR	 </b> </p>
+
+                    </div>
+                </div>
+            </div>
+
+            
+                   
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/degree.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title">B.C.A</h4>
+<p class="card-text"><b>  Intake :&nbsp; 120</b></p>
+<p class="card-text"><b>Faculty Co-Ordinator :Mr. PRAKASH BARI	</b> </p>
+
+                    </div>
+                </div>
+            </div>
+            
+            
+                   
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/degree.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title">M.B.A</h4>
+<p class="card-text"><b>  Intake :&nbsp; 180</b></p>
+<p class="card-text"><b>Faculty Co-Ordinator :Dr. Narkhede Parag Arun	</b> </p>
+
+                    </div>
+                </div>
+            </div>
+            
+                   
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/degree.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title">B.B.A</h4>
+<p class="card-text"><b>  Intake :&nbsp; 120</b></p>
+<p class="card-text"><b>Faculty Co-Ordinator :Dr. Narkhede Parag Arun	</b> </p>
+
+                    </div>
+                </div>
+            </div>
+            
+            
+                   
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/degree.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title">Integrated M.C.A</h4>
+<p class="card-text"><b>  Intake :60</b></p>
+<p class="card-text"><b>Faculty Co-Ordinator :Mrs. TANUJA FEGADE	</b> </p>
+
+                    </div>
+                </div>
+            </div>
+            
+            
+          
+ 
+
+
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Placement</h2>
+          <p> About Placement Office
+The Training and Placement Cell of KCES Institute of Management & Research, Jalgaon handles campus placement of the MCA & MBA students. The Placement Cell provides complete support to the visiting companies at every stage of placement process. Arrangements for Pre-Placement Talks, Written Tests, Interviews and Group Discussions are made as per the requirement of the visiting companies. Training & Placement Department have a well equipped Placement cell which lays a great deal of emphasis on giving corporate and students, the choice of mutual selection. Responsible for identifying the right student-organization fit, the Placement Office selects, targets and invites organizations to the campus for final placements.</p>
+<h3>Training & Placement Officer</h3>
+<p>Prof. Punit Sharma is the Training & Placement Officer for KCES Institute of Management & Research, Jalgaon. He is having 11 years of Corporate & academic training experience along with his skills on personal grooming, training and SAP enhance students overall development.
+Mobile : +91 8806936777 (Please drop a text message in case not available)
+Email : punit.sharma@imr.ac.in (Write a mail for Campus placement, off campus opportunities & Education verification)</p>
+                
+<p></p>
+            </div>
+            <div class="col-lg-6">
+                <br><br><br><br><img class="img-fluid rounded" src="images/placement.png" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+                <h4><b>Our Top Recruiters</b></h4>
+                <p>Student Placed in Supereme Industries,Axis Bank,HDFC BANK,YES BANK,Rheal Software,Mahindra Finance,SYNTEL Consider IT Done,Ladder Technologies,Assetsoft,ShriramCity,VIHAAN INFOTECH,Country Club,BHARAT Agencies,Infosys,Jain Irrigation System Ltd.,Swastika Investmart,RAYMOND,RELIGARE,Birla Sun Life Insurance,JJIT Software Solutions,Bhagirath Counseling Center,Palod Distributor,Bharat Petroleum,Legrand,ICICI PRUDENTITAL LIFE INSURANCE,RBL BANK,Nilon's,SKIT EXPERT,Perenial Technology Group,UNIVERSAL HUNT,Madhavbaug,Kotak Mutual Fund,HDFC HOME LOANS,ICICI BANK,ING BANK,NEXA,FORD,KOTAK MAHINDRA BANK,Parle Agro.</p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-student.php">Have a question?</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
